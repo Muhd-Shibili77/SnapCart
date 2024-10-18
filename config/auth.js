afecc550d7e32e38c371e1b9468983e59ee2f9dd
shibili888 <shibilishanu888@gmail.com>
2024-10-18 11:11:26 +0530

    google authencation bug resolved

--- config/auth.js
+++ config/auth.js
@@ -7,7 +7,7 @@ const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
 passport.use(new GoogleStrategy({
     clientID:process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECERT,
-    callbackURL: "http://localhost:3000/auth/google/callback",
+    callbackURL: "http://snapcart.click/auth/google/callback",
     passReqToCallback   : true
   },
   async function(request, accessToken, refreshToken, profile, done) {
